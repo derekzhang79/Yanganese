c1030bfd3fe39486bcdb45d1d37710810680baba
Michael Yang <mikkyangg@gmail.com>
2013-01-12 23:14:25 -0500

    added constants file

---
+++ Yanganese/Support/YNGAConstant.h
@@ -0,0 +1,9 @@
+//
+//  YNGAConstant.h
+//  Yanganese
+//
+//  Created by Michael Yang on 1/12/13.
+//  Copyright (c) 2013 Michael Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Yanganese/Support/YNGAConstant.m
@@ -0,0 +1,9 @@
+//
+//  YNGAConstant.m
+//  Yanganese
+//
+//  Created by Michael Yang on 1/12/13.
+//  Copyright (c) 2013 Michael Yang. All rights reserved.
+//
+
+#import "YNGAConstant.h"
