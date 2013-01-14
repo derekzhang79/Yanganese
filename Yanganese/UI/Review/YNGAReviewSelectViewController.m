a15fc490ade78b2526018b7b313591e28d3cb70f
Michael Yang <mikkyangg@gmail.com>
2013-01-14 16:56:25 -0500

    added review select controller

---
+++ Yanganese/UI/Review/YNGAReviewSelectViewController.m
@@ -0,0 +1,34 @@
+//
+//  YNGAReviewSelectViewController.m
+//  Yanganese
+//
+//  Created by Michael Yang on 1/14/13.
+//  Copyright (c) 2013 Michael Yang. All rights reserved.
+//
+
+#import "YNGAReviewSelectViewController.h"
+
+#import "YNGAAppDelegate.h"
+#import "Quiz.h"
+
+@implementation YNGAReviewSelectViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    YNGAAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Quiz" inManagedObjectContext:context];
+    
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    NSPredicate *hasScore = [NSPredicate predicateWithFormat:@"score != nil"];
+    [request setEntity:entity];
+    [request setPredicate:hasScore];
+    
+    NSError *fetchError;
+    NSArray *fetchedObjects = [context executeFetchRequest:request error:&fetchError];
+    self.quizzes = [[NSMutableArray alloc] initWithArray:fetchedObjects];
+}
+
+@end
