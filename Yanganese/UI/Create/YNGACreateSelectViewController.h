1d2470108f0de0b959acbe947e6e936eb26ab1b9
Michael Yang <mikkyangg@gmail.com>
2013-01-24 11:55:53 -0500

    added quiz upload

--- Yanganese/UI/Create/YNGACreateSelectViewController.h
+++ Yanganese/UI/Create/YNGACreateSelectViewController.h
@@ -8,6 +8,21 @@
 
 #import "YNGAQuestionSelectViewController.h"
 
-@interface YNGACreateSelectViewController : YNGAQuestionSelectViewController
+#import "YNGAAlertView.h"
+
+@class YNGAPopupView, YNGAProgressView;
+@class Quiz;
+
+@interface YNGACreateSelectViewController : YNGAQuestionSelectViewController <YNGAAlertViewDelegate>
+{
+    YNGAPopupView *popupView;
+    YNGAProgressView *progressView;
+
+    NSMutableData *retainedData;
+}
+
+@property (nonatomic, retain) Quiz *quiz;
+
+- (IBAction)uploadQuiz;
 
 @end
--- Yanganese/UI/Create/YNGACreateSelectViewController.m
+++ Yanganese/UI/Create/YNGACreateSelectViewController.m
@@ -8,9 +8,21 @@
 
 #import "YNGACreateSelectViewController.h"
 
+#import <QuartzCore/QuartzCore.h>
 #import "YNGAAppDelegate.h"
 #import "YNGACreateQuestionViewController.h"
+#import "YNGAPopupView.h"
+#import "YNGAProgressView.h"
+#import "YNGAAlertView.h"
+#import "Quiz.h"
 #import "Question.h"
+
+@interface YNGACreateSelectViewController ()
+
+- (void)uploadQuestions;
+- (void)addQuestion:(id)sender;
+
+@end
 
 @implementation YNGACreateSelectViewController
 
@@ -22,6 +34,32 @@
 
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addQuestion:)];
     self.navigationItem.rightBarButtonItem = addButton;
+    
+    // Add header view
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 78)];
+    headerView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.3f];
+
+    UIButton *finishButton = [[UIButton alloc] initWithFrame:CGRectMake(20, 20, 280, 38)];//[UIButton buttonWithType:UIButtonTypeCustom];
+    finishButton.layer.cornerRadius = kCornerRadius;
+    [finishButton setTitle:@"Upload and Finish" forState:UIControlStateNormal];
+    finishButton.titleLabel.font = [UIFont boldSystemFontOfSize:20.0f];
+    finishButton.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.7f];
+    [finishButton addTarget:self action:@selector(uploadQuiz) forControlEvents:UIControlEventTouchUpInside];
+    
+    [headerView addSubview:finishButton];
+    self.tableView.tableHeaderView = headerView;
+    
+    // Add popup view
+    popupView = [[YNGAPopupView alloc] initWithFrame:CGRectMake(20, 200, 280, 40)];
+    popupView.messageLabel.text = @"Quiz Uploaded";
+    popupView.alpha = 0.0;
+    [self.tableView addSubview:popupView];
+    
+    // Add progress view
+    progressView = [[YNGAProgressView alloc] initWithFrame:CGRectMake(20, 200, 280, 40)];
+    [progressView setProgress:0];
+    progressView.alpha = 0.0;
+    [self.tableView addSubview:progressView];
     
     self.data = [[NSMutableArray alloc] init];
 }
@@ -70,6 +108,94 @@
 
 #pragma mark -
 
+- (IBAction)uploadQuiz
+{
+    // Save quiz locally
+    YNGAAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    
+    [context insertObject:_quiz];
+    
+    for(Question *question in self.data)
+        [_quiz addQuestionsObject:question];
+    
+    [context save:nil];
+    
+    // Upload quiz
+    NSDictionary *quizDict = @{@"quiz":[_quiz properties]};
+    NSData *data = [NSJSONSerialization dataWithJSONObject:quizDict options:kNilOptions error:nil];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:kQuizURL]];
+    [request setHTTPMethod:@"POST"];
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:data];
+    
+    NSURLResponse *response;
+    NSError *error;
+    NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    if(responseData)
+    {
+        NSDictionary *jsonResponse = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:nil];
+        _quiz.quizID = [jsonResponse objectForKey:@"quiz_id"];
+        [context save:nil];
+        
+        void(^uploadQuestions)(BOOL) = ^(BOOL completion) {
+            [self uploadQuestions];
+        };
+        
+        [UIView animateWithDuration:kTransitionTime animations:^{
+            progressView.alpha = 1.0f;
+        } completion:uploadQuestions];
+    }
+    else
+    {
+    }
+}
+
+- (void)uploadQuestions
+{
+    // Upload questions
+    NSInteger questionCount = 0;
+    for(Question *question in _quiz.questions)
+    {
+        NSDictionary *quizDict = @{@"question":[question properties]};
+        NSData *data = [NSJSONSerialization dataWithJSONObject:quizDict options:kNilOptions error:nil];
+        
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:kQuestionURL]];
+        [request setHTTPMethod:@"POST"];
+        [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+        [request setHTTPBody:data];
+        
+        NSURLResponse *response;
+        NSError *error;
+        NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+        
+        if(responseData)
+        {
+            questionCount++;
+            [progressView setProgress:(questionCount * 1.0f / _quiz.questions.count)];
+        }
+    }
+    
+    // Fade and reset progress
+    void(^fadeProgress)(void) = ^ {
+        progressView.alpha = 0.0f;
+    };
+    
+    void(^clearProgress)(BOOL) = ^(BOOL completion) {
+        YNGAAlertView *alertView = [[YNGAAlertView alloc] initWithFrame:CGRectMake(30, 120, 260, 200)];
+        alertView.cancelButton.hidden = YES;
+        alertView.messageLabel.text = @"Upload finished. You will now be taken back to the main menu.";
+        alertView.delegate = self;
+        
+        [self.view addSubview:alertView];
+        [alertView show];
+    };
+    
+    [UIView animateWithDuration:kTransitionTime animations:fadeProgress completion:clearProgress];
+}
+
 - (void)addQuestion:(id)sender
 {
     [self performSegueWithIdentifier:@"addQuestion" sender:sender];
@@ -99,6 +225,26 @@
         [context deleteObject:question];
         
         [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationLeft];
+    }
+}
+
+#pragma mark - Alert view delegate
+
+- (void)alertView:(YNGAAlertView *)alertView didDismissWithButtonIndex:(ButtonIndex)buttonIndex
+{
+    if(buttonIndex == kOKButtonIndex)
+    {
+        void(^fadeView)(void) = ^ {
+            [self.navigationController setNavigationBarHidden:YES animated:YES];
+            for(UIView *view in self.view.subviews)
+                view.alpha = 0.0f;
+        };
+        
+        void(^popToRoot)(BOOL) = ^(BOOL completion) {
+            [self.navigationController popToRootViewControllerAnimated:NO];
+        };
+        
+        [UIView animateWithDuration:kTransitionTime animations:fadeView completion:popToRoot];
     }
 }
 
