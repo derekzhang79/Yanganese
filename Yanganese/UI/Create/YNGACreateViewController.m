8d8eba0cbc759f5e30d89ac87d97845bf9595eb1
Michael Yang <mikkyangg@gmail.com>
2013-01-26 19:49:52 -0500

    added table view data and delegate

--- Yanganese/UI/Create/YNGACreateViewController.m
+++ Yanganese/UI/Create/YNGACreateViewController.m
@@ -8,6 +8,11 @@
 
 #import "YNGACreateViewController.h"
 
+#import "YNGAAppDelegate.h"
+#import "YNGACreateQuizViewController.h"
+#import "YNGACreateQuestionSelectViewController.h"
+#import "Quiz.h"
+
 @interface YNGACreateViewController ()
 
 - (void)addQuiz:(id)sender;
@@ -22,6 +27,62 @@
     
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addQuiz:)];
     self.navigationItem.rightBarButtonItem = addButton;
+    
+    // Load quizzes
+    YNGAAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Quiz" inManagedObjectContext:context];
+    
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    NSPredicate *notUploaded = [NSPredicate predicateWithFormat:@"quizID == nil"];
+    [request setEntity:entity];
+    [request setPredicate:notUploaded];
+    
+    NSError *fetchError;
+    NSArray *fetchedObjects = [context executeFetchRequest:request error:&fetchError];
+    self.data = [[NSMutableArray alloc] initWithArray:fetchedObjects];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self.tableView reloadData];
+
+    [super viewWillAppear:animated];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if([segue.identifier isEqualToString:@"addQuiz"])
+    {
+        YNGAAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+        NSManagedObjectContext *context = [appDelegate managedObjectContext];
+        NSEntityDescription *entity = [NSEntityDescription entityForName:@"Quiz" inManagedObjectContext:context];
+        
+        Quiz *quiz = [[Quiz alloc] initWithEntity:entity insertIntoManagedObjectContext:nil];
+        
+        YNGACreateQuizViewController *createController = segue.destinationViewController;
+        createController.quiz = quiz;
+        
+        [self.data addObject:quiz];
+    }
+    else if([segue.identifier isEqualToString:@"editQuiz"])
+    {
+        NSIndexPath *indexPath;
+        Quiz *quiz;
+        if(sender == self.searchDisplayController.searchResultsTableView)
+        {
+            indexPath = [self.tableView indexPathForSelectedRow];
+            quiz = [self.filteredData objectAtIndex:[indexPath row]];
+        }
+        else
+        {
+            indexPath = [self.searchDisplayController.searchResultsTableView indexPathForSelectedRow];
+            quiz = [self.data objectAtIndex:[indexPath row]];
+        }
+        
+        YNGACreateQuestionSelectViewController *createSelectController = segue.destinationViewController;
+        createSelectController.quiz = quiz;
+    }
 }
 
 #pragma mark -
@@ -29,6 +90,15 @@
 - (void)addQuiz:(id)sender
 {
     [self performSegueWithIdentifier:@"addQuiz" sender:sender];
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self performSegueWithIdentifier:@"editQuiz" sender:tableView];
+    
+    [super tableView:tableView didSelectRowAtIndexPath:indexPath];
 }
 
 @end
